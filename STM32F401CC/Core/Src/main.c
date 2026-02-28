47ae15754d8644d83b846f0cbf875f22253c9d84
Vel11leV <mishchanchukvp@gmail.com>
2026-02-28 03:24:05 +0100

    debounce and short/long pushes

--- STM32F401CC/Core/Src/main.c
+++ STM32F401CC/Core/Src/main.c
@@ -47,14 +47,79 @@ TIM_HandleTypeDef htim1;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+static GPIO_PinState s_pinStatePrevious;
+static GPIO_PinState s_pinStateNew;
+static uint8_t buttonDebounce = 0;
+
+
+#define DEBOUNCE_TIMEOUT 200
+
+volatile uint32_t button_press_time = 0;
+
+void buttonShort(void) {
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_9);
+}
+void buttonLong(void) {
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_10);
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+    if (htim->Instance == TIM1) {
+
+        HAL_TIM_Base_Stop_IT(htim);
+        __HAL_TIM_SET_COUNTER(htim, 0);
+        s_pinStatePrevious = s_pinStateNew;
+        s_pinStateNew = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_11);
+        if (s_pinStateNew == s_pinStatePrevious) {
+            buttonDebounce = 1;
+            button_press_time = HAL_GetTick();
+
+            HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+        } else {
+            buttonDebounce = 0;
+            HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+        }
+    }
+}
+
+
+ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+     if(GPIO_Pin == GPIO_PIN_11){
+         HAL_NVIC_DisableIRQ(EXTI15_10_IRQn);
+         if (!buttonDebounce) {
+
+         s_pinStateNew = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_11);
+         __HAL_TIM_SET_COUNTER(&htim1, 0);
+         TIM1->ARR = DEBOUNCE_TIMEOUT;
+         HAL_TIM_Base_Start_IT(&htim1);
+
+         } else {
+             uint32_t currentTick = HAL_GetTick();
+             uint32_t duration = currentTick - button_press_time;
+
+             if (duration > 2000) {
+                 buttonLong();
+             } else {
+                 buttonShort();
+             }
+
+             buttonDebounce = 0;
+             HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+         }
+
+     }
+}
 
 /* USER CODE END 0 */
 
@@ -85,19 +150,23 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
   MX_TIM1_Init();
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_1);
-  HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_2);
-  HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_3);
-  HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_4);
+  //HAL_TIM_Base_Start_IT(&htim1);
+  //__HAL_TIM_ENABLE_IT(&htim1, TIM_IT_CC1);
+  //HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_1);
+  //HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_2);
+  //HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_3);
+  //HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_4);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+      s_pinStateNew = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_11);
       HAL_Delay(500);
     /* USER CODE END WHILE */
 
@@ -159,19 +228,17 @@ static void MX_TIM1_Init(void)
 
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
-  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
 
   /* USER CODE BEGIN TIM1_Init 1 */
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 16000;
+  htim1.Init.Prescaler = 0;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 1000;
+  htim1.Init.Period = 65535;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
-  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
   {
     Error_Handler();
@@ -181,57 +248,15 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  if (HAL_TIM_OC_Init(&htim1) != HAL_OK)
-  {
-    Error_Handler();
-  }
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
   if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  sConfigOC.OCMode = TIM_OCMODE_TOGGLE;
-  sConfigOC.Pulse = 1000;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
-  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
-  if (HAL_TIM_OC_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigOC.Pulse = 500;
-  if (HAL_TIM_OC_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigOC.Pulse = 250;
-  if (HAL_TIM_OC_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigOC.Pulse = 125;
-  if (HAL_TIM_OC_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
-  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
-  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
-  sBreakDeadTimeConfig.DeadTime = 0;
-  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
-  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
-  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
-  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
   /* USER CODE BEGIN TIM1_Init 2 */
 
   /* USER CODE END TIM1_Init 2 */
-  HAL_TIM_MspPostInit(&htim1);
 
 }
 
@@ -242,9 +267,30 @@ static void MX_TIM1_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA8 PA9 PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 }
 
